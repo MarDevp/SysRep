893a077741dde273e5904cf34cc1fe793f978f61
MarDevp <zarroukimariem01@gmail.com>
2024-02-13 21:11:20 +0100

    Create ClientUDP.java

---
+++ ClientUDP.java
@@ -0,0 +1,20 @@
+public class UDPClient {
+    public static void main(String[] args) {
+        DatagramSocket clientSocket = null;
+        
+        try {
+            // Création d'une socket UDP
+            clientSocket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] sendData = "Hello, serveur UDP!".getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            clientSocket.send(sendPacket);
+            System.out.println("Message envoyé.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (clientSocket != null)
+                clientSocket.close();
+        }
+    }
+}
